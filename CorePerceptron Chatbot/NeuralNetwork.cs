2b14205ed8344effb62b994ca6a2d6abdb9de2ee
Li Smith <lrhude.elijah@gmail.com>
2017-10-19 21:56:08 -0400

    Added base of backpropagation

--- CorePerceptron Chatbot/NeuralNetwork.cs
+++ CorePerceptron Chatbot/NeuralNetwork.cs
@@ -61,11 +61,11 @@ namespace CorePerceptron_Chatbot
 
                 if (returnMessage.Count > 0)
                 {
-                    returnMessage.Add(wordToOutput(ChatbotCore.WordToNumber(word), ChatbotCore.WordToNumber(returnMessage[returnMessage.Count - 1])));
+                    returnMessage.Add(forwardPropagate(ChatbotCore.WordToNumber(word), ChatbotCore.WordToNumber(returnMessage[returnMessage.Count - 1])));
                 }
                 else
                 {
-                    returnMessage.Add(wordToOutput(ChatbotCore.WordToNumber(word), -1));
+                    returnMessage.Add(forwardPropagate(ChatbotCore.WordToNumber(word), -1));
                 }
             }
 
@@ -84,7 +84,7 @@ namespace CorePerceptron_Chatbot
         }
 
         //communicate with perceptrons
-        private String wordToOutput(float word, float lastWordSet)
+        private String forwardPropagate(float word, float lastWordSet)
         {
             float value = 0.0F;
 
@@ -108,14 +108,108 @@ namespace CorePerceptron_Chatbot
             }
             
             value = (float)Math.Round(inputs[0], 2);
-            Console.WriteLine(value);
             return ChatbotCore.NumberToWord(value);
         }
 
         //train perceptrons
         private void trainPerceptrons(String user1Words, String user2Words)
         {
+            //iterate over words
+            List<String> inputWords = new List<String>();
+            List<String> targetWords = new List<String>();
+            String currentWord = "";
 
+            for (int index = 0; index < user1Words.Length; index++)
+            {
+                String c = user1Words.Substring(index, 1);
+
+                if (c.Equals(" "))
+                {
+                    inputWords.Add(currentWord);
+                    currentWord = "";
+                }
+                else
+                {
+                    currentWord += c;
+                }
+            }
+
+            for (int index = 0; index < user2Words.Length; index++)
+            {
+                String c = user2Words.Substring(index, 1);
+
+                if (c.Equals(" "))
+                {
+                    targetWords.Add(currentWord);
+                    currentWord = "";
+                }
+                else
+                {
+                    currentWord += c;
+                }
+            }
+
+            //iterate over longer list of words for training
+            for (int index = 0; index < (inputWords.Count > targetWords.Count ? targetWords.Count : inputWords.Count); index++)
+            {
+                float inputWord = ChatbotCore.WordToNumber(inputWords[index]);
+                float targetWord = ChatbotCore.WordToNumber(targetWords[index]);
+
+                backPropagateData(inputWord, targetWord);
+            }
+        }
+
+        //back propagate data
+        private void backPropagateData(float inputWord, float targetWord)
+        {
+            //iterate over layers
+            float[] error = new float[2];
+
+            for (int layerIndex = perceptrons.Count - 1; layerIndex > 1; layerIndex--)
+            {
+                List<Perceptron> layer = perceptrons[layerIndex];
+                float[] errorBuffer = new float[layer.Count];
+
+                //iterate over neurons
+                for (int neuronIndex = 0; neuronIndex < layer.Count; neuronIndex++)
+                {
+                    Perceptron neuron = layer[neuronIndex];
+
+                    //fetch inputs with forward propagation
+                    float[] inputs = new float[2];
+
+                    //iterate over layers
+                    for (int indexOfPropagateLayer = 0; indexOfPropagateLayer < layerIndex; indexOfPropagateLayer++)
+                    {
+                        List<Perceptron> layerToPropagate = perceptrons[indexOfPropagateLayer];
+                        float[] inputsBuffer = new float[layerToPropagate.Count];
+
+                        //iterate over neurons
+                        for (int indexOfNeuronToPropagate = 0; indexOfNeuronToPropagate < layerToPropagate.Count; indexOfNeuronToPropagate++)
+                        {
+                            inputsBuffer[indexOfNeuronToPropagate] = layerToPropagate[indexOfNeuronToPropagate].output(inputs);
+                        }
+
+                        inputs = inputsBuffer;
+                    }
+
+                    //train
+                    if (layerIndex == perceptrons.Count - 1)
+                    {
+                        error[neuronIndex] = neuron.train(inputs, targetWord, false);
+                    }
+                    else
+                    {
+                        //sum error 
+                        float errorToPassToNetwork = 0;
+
+                        //pass to network
+                        error[neuronIndex] = neuron.train(inputs, errorToPassToNetwork, true);
+                    }
+                }
+
+                error = errorBuffer;
+            }
         }
     }
 }
